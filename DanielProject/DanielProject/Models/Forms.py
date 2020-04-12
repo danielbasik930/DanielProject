84e62b3d0308dce92856574194840ef1fb2231f6
דניאל בסיק תשתש <Danielbasik@gmail.com>
2020-04-12 10:21:47 +0300

    Add project files.

---
+++ DanielProject/DanielProject/Models/Forms.py
@@ -0,0 +1,22 @@
+from datetime import datetime
+
+from flask_wtf import FlaskForm
+from wtforms import StringField, SubmitField
+from wtforms import Form, BooleanField, PasswordField , HiddenField , DateTimeField , IntegerField , DecimalField , FloatField , RadioField
+from wtforms import Form, SelectMultipleField , BooleanField
+from wtforms import TextField, TextAreaField, SelectField
+from wtforms import validators, ValidationError
+from wtforms.fields.html5 import DateField
+
+from wtforms.validators import DataRequired
+from wtforms.validators import InputRequired
+
+class ExpandForm(FlaskForm):
+	submit1 = SubmitField('Expand')
+	name="Expand"
+	value="Expand" 
+class CollapseForm(FlaskForm):
+	submit2 = SubmitField('Collapse')
+	name="Collapse"
+	value="Collapse" 
+
---
+++ DanielProject/DanielProject/views.py
@@ -0,0 +1,236 @@
+"""
+Routes and views for the flask application.
+"""
+
+from datetime import datetime
+from flask import render_template
+from DanielProject import app
+from DanielProject.Models.LocalDatabaseRoutines import create_LocalDatabaseServiceRoutines
+
+
+from datetime import datetime
+from flask import render_template, redirect, request
+
+from flask_wtf import FlaskForm
+from wtforms import StringField, SubmitField
+from wtforms.validators import DataRequired
+
+
+import numpy as np
+import matplotlib.pyplot as plt
+import pandas as pd
+
+import json 
+import requests
+
+import io
+import base64
+
+from os import path
+
+from flask   import Flask, render_template, flash, request
+from wtforms import Form, BooleanField, StringField, PasswordField, validators
+from wtforms import TextField, TextAreaField, SubmitField, SelectField, DateField
+from wtforms import ValidationError
+
+
+from DanielProject.Models.QueryFormStructure import QueryFormStructure 
+from DanielProject.Models.QueryFormStructure import LoginFormStructure 
+from DanielProject.Models.QueryFormStructure import UserRegistrationFormStructure 
+
+###from DemoFormProject.Models.LocalDatabaseRoutines import IsUserExist, IsLoginGood, AddNewUser 
+
+db_Functions = create_LocalDatabaseServiceRoutines() 
+SECRET_KEY = 'my super secret key'.encode('utf8')
+
+@app.route('/')
+@app.route('/home')
+def home():
+    """Renders the home page."""
+    return render_template(
+        'index.html',
+        title='Home',
+        year=datetime.now().year,
+    )
+
+@app.route('/contact')
+def contact():
+    """Renders the contact page."""
+    return render_template(
+        'contact.html',
+        title='Contact',
+        year=datetime.now().year,
+        message=''
+    )
+
+@app.route('/about')
+def about():
+    """Renders the about page."""
+    return render_template(
+        'about.html',
+        title='',
+        year=datetime.now().year,
+        message=''
+    )
+
+
+@app.route('/Album')
+def Album():
+    """Renders the about page."""
+    return render_template(
+        'PictureAlbum.html',
+        title='Pictures',
+        year=datetime.now().year,
+        message='Welcome to my picture album'
+    )
+
+
+@app.route('/Query', methods=['GET', 'POST'])
+def Query():
+
+    Name = None
+    Country = ''
+    capital = ''
+    df = pd.read_csv(path.join(path.dirname(__file__), 'static\\Data\\toprun.csv'))
+    df = df.set_index('Country')
+
+    raw_data_table = df.to_html(classes = 'table table-hover')
+
+    form = QueryFormStructure(request.form)
+     
+    if (request.method == 'POST' ):
+        name = form.name.data
+        Country = name
+        if (name in df.index):
+            capital = df.loc[name,'Capital']
+            raw_data_table = ""
+        else:
+            capital = name + ', no such country'
+        form.name.data = ''
+
+
+
+    return render_template('Query.html', 
+            form = form, 
+            name = capital, 
+            Country = Country,
+            raw_data_table = raw_data_table,
+            title='Query by the user',
+            year=datetime.now().year,
+            message='This page will use the web forms to get user input'
+        )
+
+# -------------------------------------------------------
+# Register new user page
+# -------------------------------------------------------
+@app.route('/register', methods=['GET', 'POST'])
+def Register():
+    form = UserRegistrationFormStructure(request.form)
+
+    if (request.method == 'POST' and form.validate()):
+        if (not db_Functions.IsUserExist(form.username.data)):
+            db_Functions.AddNewUser(form)
+            db_table = ""
+
+            flash('Thanks for registering new user - '+ form.FirstName.data + " " + form.LastName.data )
+            # Here you should put what to do (or were to go) if registration was good
+        else:
+            flash('Error: User with this Username already exist ! - '+ form.username.data)
+            form = UserRegistrationFormStructure(request.form)
+
+    return render_template(
+        'register.html', 
+        form=form, 
+        title='Register New User',
+        year=datetime.now().year,
+        repository_name='Pandas',
+        )
+
+# -------------------------------------------------------
+# Login page
+# This page is the filter before the data analysis
+# -------------------------------------------------------
+@app.route('/login', methods=['GET', 'POST'])
+def Login():
+    form = LoginFormStructure(request.form)
+
+    if (request.method == 'POST' and form.validate()):
+        if (db_Functions.IsLoginGood(form.username.data, form.password.data)):
+            flash('Login approved!')
+            #return redirect('<were to go if login is good!')
+        else:
+            flash('Error in - Username and/or password')
+   
+    return render_template(
+        'login.html', 
+        form=form, 
+        title='Login to data analysis',
+        year=datetime.now().year,
+        repository_name='Pandas',
+        )
+
+
+
+@app.route('/DataModel')
+def DataModel():
+    """Renders the contact page."""
+    return render_template(
+        'DataModel.html',
+        title='This is my Data Model page abou top runnig ',
+        year=datetime.now().year,
+        message='In this page we will display the datasets we are going to   '
+    )
+
+
+
+
+from flask_bootstrap import Bootstrap
+bootstrap = Bootstrap(app)
+
+from DanielProject.Models.Forms import ExpandForm
+from DanielProject.Models.Forms import CollapseForm
+
+@app.route('/data/toprun' , methods = ['GET' , 'POST'])
+def DataSet1():
+    form1 = ExpandForm()
+    form2 = CollapseForm()
+    df = pd.read_csv(path.join(path.dirname(__file__), 'static\\data\\toprun.csv'))
+    raw_data_table = ''
+    
+    if request.method == 'POST':
+        if request.form['action'] == 'Expand' and form1.validate_on_submit():
+            raw_data_table = df.to_html(classes = 'table table-hover')
+        if request.form['action'] == 'Collapse' and form2.validate_on_submit():
+            raw_data_table = ''
+
+    return render_template(
+        'DataSet1.html',
+        title='The Run Results records',
+        year=datetime.now().year ,
+        raw_data_table = raw_data_table,
+        form1 = form1,
+        form2 = form2
+    )
+@app.route('/data/citiy_csv_short_from_oran' , methods = ['GET' , 'POST'])
+def DataSet():
+    form1 = ExpandForm()
+    form2 = CollapseForm()
+    df = pd.read_csv(path.join(path.dirname(__file__), 'static\\data\\citiy_csv_short_from_oran.csv'))
+    raw_data_table = ''
+    
+    if request.method == 'POST':
+        if request.form['action'] == 'Expand' and form1.validate_on_submit():
+            raw_data_table = df.to_html(classes = 'table table-hover')
+        if request.form['action'] == 'Collapse' and form2.validate_on_submit():
+            raw_data_table = ''
+
+    return render_template(
+        'DataSet.html',
+        title='The Run Results records',
+        year=datetime.now().year ,
+        raw_data_table = raw_data_table,
+        form1 = form1,
+        form2 = form2
+    )
+
+
